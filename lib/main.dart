537dcf2452d1a962e125e7032c15311159d16db7
Bruno Silva <brunoguilhermev7@gmail.com>
2019-10-29 19:11:28 +0000

    Changed home page layout, and included bottom navigation drawer (Home, Lookup and Trips). Included different colours for each page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,252 +1,101 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static const String _title = 'Silver Dawn Coaches';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
+      title: _title,
+      home: MyStatefulWidget(),
     );
   }
 }
 
-class HomePage extends StatefulWidget{
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
   @override
-  _HomePageState createState() => _HomePageState();
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
-class _HomePageState extends State<HomePage>{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(255, 3, 3, 0.5),
+class PlaceholderWidget extends StatelessWidget {
+  final Color color;
 
-        ), child: BottomNavigator(),
-      ),
-    );
-  }
-}
-
-class BottomNavigator extends StatelessWidget {
+  PlaceholderWidget(this.color);
 
   @override
   Widget build(BuildContext context) {
-    return new Stack(
-      children: <Widget>[
-        new Positioned(
-            child: Column(
-              children: <Widget>[
-                new Container(
-                  alignment: Alignment.bottomCenter,
+    return Container(
+      color: color,
+    );
+  }
+}
 
-                  child: Card( // TODO: Create logout, statistics and user profile button at the top (With row)
-                    margin: EdgeInsets.zero,
-                    clipBehavior: Clip.antiAlias,
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
 
-                    shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.only(
-                          bottomLeft: const Radius.circular(20.0),
-                          bottomRight: const Radius.circular(20.0)),
+  final List<Widget> _widgetOptions = [
+    PlaceholderWidget(Colors.yellow),
+    PlaceholderWidget(Colors.deepOrange),
+    PlaceholderWidget(Colors.green)
+  ];
 
-                      side: BorderSide(
-                        color: Colors.white,
-                        width: 3,
-                      ),
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
-                    ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          
+          Center(child: _widgetOptions.elementAt(_selectedIndex)),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: bottomNavigationBar,
+          ),
+        ],
+      ),
 
-                    color: Colors.black87,
-                    child: Column(
-                      children: <Widget>[
-                        new Row(
-                          children: <Widget>[
-                            Container(
-                              margin: const EdgeInsets.only(left: 40.0, top: 30.0),
-                              width: 50,
-                              height: 50,
-                              child: new Container(
-                                  child: new Image.asset(
-                                    'assets/round_exit_to_app_white_18.png',
-                                    height: 30.0,
-                                    fit: BoxFit.cover,
-                                  )
+    );
+  }
 
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(right: 40.0, top: 30.0),
-                              width: 50,
-                              height: 50,
-                              child: new Container(
-                                child: new Image.asset(
-                                  'assets/open_graph.png',
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Container(
-                            width: MediaQuery.of(context).size.width,
-                            alignment: Alignment.bottomCenter,
-                            height: MediaQuery.of(context).size.height * 0.36,
+  Widget get bottomNavigationBar {
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topRight: Radius.circular(40),
+        topLeft: Radius.circular(40),
+      ),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.zoom_in),
+              title: Text('Lookup'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.card_travel),
+              title: Text('Trips'),
+            ),
+          ],
 
-                            child: new Container(
-                              margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10.0, top: 20.0),
-                              width: MediaQuery.of(context).size.width,
-                              height: double.infinity,
-
-                              child: new Card(
-                                  shape: RoundedRectangleBorder(
-
-                                    borderRadius: new BorderRadius.only(
-                                      bottomLeft: const Radius.circular(20.0),
-                                      bottomRight: const Radius.circular(20.0),
-                                      topLeft: const Radius.circular(20.0),
-                                      topRight: const Radius.circular(20.0),
-                                    ),
-
-                                    side: BorderSide(
-                                      color: Colors.grey,
-                                      width: 5,
-                                    ),
-                                  ),
-                                  child: new Column(
-                                    children: <Widget>[
-                                      Expanded(
-                                        child: Container(
-                                          margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 0.0, top: 18.0),
-                                          width: MediaQuery.of(context).size.width,
-
-                                          child: GestureDetector(
-                                            onTap: () {  // TODO: Open a popup to create new Party.
-                                              Scaffold.of(context).showSnackBar(SnackBar(
-                                                content: Text('Unable to create a new party.'),
-                                              ));
-                                            },
-                                            child: Card(
-                                                elevation: 10,
-                                                color: Color.fromRGBO(36, 41, 80, 1),
-                                                shape: RoundedRectangleBorder(
-
-                                                  borderRadius: new BorderRadius.only(
-                                                    bottomLeft: const Radius.circular(20.0),
-                                                    bottomRight: const Radius.circular(20.0),
-                                                    topLeft: const Radius.circular(20.0),
-                                                    topRight: const Radius.circular(20.0),
-                                                  ),
-
-                                                  side: BorderSide(
-                                                    color: Colors.white,
-                                                    width: 5,
-                                                  ),
-
-                                                ),
-
-                                                child: new Row(
-                                                  mainAxisAlignment: MainAxisAlignment.center,
-                                                  children: <Widget>[
-                                                    new Container(
-                                                        child: new Image.asset(
-                                                          'assets/round_gamepad_white_18.png',
-                                                          height: 50.0,
-                                                          fit: BoxFit.cover,
-                                                        )
-                                                    ), Container(
-
-                                                      margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 0.0, top: 0.0),
-                                                      child: new Text(
-                                                        "Create a Party",
-                                                        style: new TextStyle(
-                                                          fontSize: 20.0,
-                                                          color: Colors.white,
-                                                        ),
-                                                      ),
-                                                    )
-                                                  ],
-                                                )
-
-                                            ),
-                                          ),
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped,
 
 
-
-                                        ),
-                                      ),
-                                      Expanded(
-                                        child: Container(
-                                          margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0, top: 0.0),
-                                          width: MediaQuery.of(context).size.width,
-
-                                          child: Card(
-                                              color: Color.fromRGBO(30, 30, 30, 1),
-                                              shape: RoundedRectangleBorder(
-
-                                                borderRadius: new BorderRadius.only(
-                                                  bottomLeft: const Radius.circular(20.0),
-                                                  bottomRight: const Radius.circular(20.0),
-                                                  topLeft: const Radius.circular(20.0),
-                                                  topRight: const Radius.circular(20.0),
-                                                ),
-
-
-                                              ),
-
-                                              child: new Row(
-                                                mainAxisAlignment: MainAxisAlignment.center,
-                                                children: <Widget>[
-                                                  new Container(
-                                                      child: new Image.asset(
-                                                        'assets/round_group_white_18.png',
-                                                        height: 50.0,
-                                                        fit: BoxFit.cover,
-                                                      )
-                                                  ), Container(
-
-                                                    margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 0.0, top: 0.0),
-                                                    child: new Text(
-                                                      "Join a Party",
-                                                      style: new TextStyle(
-                                                        fontSize: 20.0,
-                                                        color: Colors.white,
-                                                      ),
-                                                    ),
-                                                  )
-                                                ],
-                                              )
-
-                                          ),
-
-
-                                        ),
-                                      ),
-
-                                    ],
-                                  )
-
-                              ),
-                            )
-                        ),
-                      ],
-                    ),
-
-                  ),
-                ),
-
-
-              ],
-            )
-        ),
-      ],
+      ),
     );
   }
 }
